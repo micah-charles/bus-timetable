838d2168e2bd523b66ae08c95926b3d10888668d
micah-charles <ckstks@yahoo.com>
2025-05-05 00:10:06 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+    "name": "bus-times-server",
+    "version": "1.0.0",
+    "description": "Server for Cheam Broadway bus times",
+    "main": "server.js",
+    "scripts": {
+        "start": "node server.js"
+    },
+    "dependencies": {
+        "express": "^4.17.1",
+        "node-fetch": "^2.6.1"
+    },
+    "engines": {
+        "node": ">=14"
+    }
+}
